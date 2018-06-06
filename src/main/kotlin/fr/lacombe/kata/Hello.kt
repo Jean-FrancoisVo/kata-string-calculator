21f3f4f853392736d38c4568cd4a8501f19bc4c9
Jean-François VO <jean-francois-externe.vo@edf.fr>
2018-06-06 15:08:23 +0200

    Initial commit

---
+++ src/main/kotlin/fr/lacombe/kata/Hello.kt
@@ -0,0 +1,6 @@
+package fr.lacombe.kata
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/fr/lacombe/kata/HelloTest.kt
@@ -0,0 +1,8 @@
+package fr.lacombe.kata
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}

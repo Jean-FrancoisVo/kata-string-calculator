a8e70bf320243f9a4565c05454989bbb8f48ad9d
Jean-François VO <jean-francois-externe.vo@edf.fr>
2018-06-11 17:11:02 +0200

    Step 2 - 3, 4 or more numbers

--- src/test/kotlin/fr/lacombe/kata/StringCalculatorTest.kt
+++ src/test/kotlin/fr/lacombe/kata/StringCalculatorTest.kt
@@ -3,7 +3,7 @@ package fr.lacombe.kata
 import org.junit.Test
 import kotlin.test.assertEquals
 
-class HelloTest {
+class StringCalculatorTest {
     @Test
     fun `it can contain 0 number`() {
         assertEquals(add(""), 0)
@@ -18,12 +18,19 @@ class HelloTest {
     fun `it can contain 2 numbers`() {
         assertEquals(add("1,2"), 3)
     }
+
+    @Test
+    fun `it can contain 3 and 4 numbers or more`() {
+        assertEquals(add("1,2,3"), 6)
+        assertEquals(add("1,2,3,3"), 9)
+        assertEquals(add("1,2,3,3,3"), 12)
+    }
 }
 
 fun add(values: String): Int {
     if (values.isEmpty())
         return 0
-    val split = values.split(",")
+    return values.split(",")
             .map { e -> e.toInt() }
-    return if (split.size == 1) split[0] else split[0] + split[1]
+            .reduce { acc, n -> acc + n }
 }
